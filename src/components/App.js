f7506518ed41837d4bf0fe8eee9e773bcc34112a
eb0687 <ebrahim0687@gmail.com>
2025-04-23 12:04:04 +0300

    feat: add clear button

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import { elements } from "../../framework";
+import { Footer } from "./Footer";
 import { Header } from "./Header";
 import { TodoInput } from "./TodoInput";
 import { TodoList } from "./TodoList";
@@ -6,5 +7,11 @@ import { TodoList } from "./TodoList";
 const { div } = elements;
 
 export function App() {
-  return div({ class: "todo-app" }, Header(), TodoInput(), TodoList());
+  return div(
+    { class: "todo-app" },
+    Header(),
+    TodoInput(),
+    TodoList(),
+    Footer(),
+  );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,30 @@
+import { div, button } from "../../framework";
+import { filter, todos } from "../main";
+
+export function Footer() {
+  const filters = ["all", "active", "completed"];
+
+  const buttons = filters.map((name) =>
+    button(
+      {
+        class: "btn",
+        onClick: () => {
+          filter.value = name;
+        },
+      },
+      name[0].toUpperCase() + name.slice(1),
+    ),
+  );
+
+  const clearButton = button(
+    {
+      class: "btn",
+      onClick: () => {
+        todos.value = todos.value.filter((todo) => !todo.completed);
+      },
+    },
+    "Clear completed",
+  );
+
+  return div({ class: "footer" }, ...buttons, clearButton);
+}
