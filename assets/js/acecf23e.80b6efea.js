d4b8de95ae953308d08ca3162348dca88611a7f6
eb0687 <ebrahim0687@gmail.com>
2025-04-29 12:17:11 +0300

    Deploy website - based on 092aac115811b07e6429dbf58be9f765afabd524

---
+++ assets/js/acecf23e.80b6efea.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[903],{1912:o=>{o.exports=JSON.parse('{"blogBasePath":"/mini-framework/docs/docs/intro/blog","blogTitle":"Blog","authorsListPath":"/mini-framework/docs/docs/intro/blog/authors"}')}}]);
